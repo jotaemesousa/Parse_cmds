d87f0b1cb90400a97067614cfae4525f507c9982
João Sousa <sousa1989joao@gmail.com>
2013-12-05 23:20:52 +0000

    first, a seguir este snippet

---
+++ main.cpp
@@ -0,0 +1,55 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 25, 2013
+ *      Author: joao
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef enum
+{
+	IDLE_State, CMD_ID_State, N_AGR_State, Parse_State
+}ParseState;
+
+int main(int argc, char **argv)
+{
+	char input;
+
+	ParseState currentState = IDLE_State;
+
+	while(1)
+	{
+		while(scanf("%c", &input))
+		{
+			printf("echo |%c|\n", input);
+
+			switch (currentState)
+			{
+			case IDLE_State:
+
+				if (input=='@')
+				{
+					currentState = CMD_ID_State;
+				}
+				break;
+
+
+
+
+
+
+			default:
+				break;
+			}
+
+		}
+	}
+
+
+	return 0;
+}
